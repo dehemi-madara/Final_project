42b12a61b2ef0147b305c095d1af1bbe1dd35565
dehemi-madara <dehemimadara@gmail.com>
2024-01-13 10:58:53 +0530

    completed order part

--- flowerShop/admin_orders.php
+++ flowerShop/admin_orders.php
@@ -11,6 +11,27 @@
         header('location:login.php');
     }
 
+     /*--------------------deleting orders details from database---------------------- */
+     if(isset($_GET['delete'])) {
+        $delete_id = $_GET['delete'];
+
+        mysqli_query($conn, "DELETE FROM `orders` WHERE id = '$delete_id'") or die('query failed');
+
+        header('location:admin_orders.php');
+    }
+
+     /*-------------- update order details --------------- */
+
+     if(isset($_POST['update_order'])) {
+        $order_id = $_POST['order_id'];
+        $update_payment = $_POST['update_payment'];
+
+        mysqli_query($conn, "UPDATE `orders` SET payment_status='$update_payment' WHERE id='$order_id'") or die('query failed');
+        $message[]='payment status updated successfully';
+    }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,6 +43,7 @@
     crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet"type = "text/css"href = "styleadmin.css">
+    <link rel="stylesheet"type="text/css"href= "styleorder.css">
     <title>admin pannel</title>
 </head>
 <body>
