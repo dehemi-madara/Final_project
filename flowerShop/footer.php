f7c64df72eee77f91490c811eeca10448032b499
sewmini-madhuwanthi <sewminim107@gmail.com>
2024-02-13 12:32:26 +0530

    g

---
+++ flowerShop/footer.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="home-contact">
+        <h1>have any question?</h1>
+        <p>Lohjfegbr34mejo43r gfjokmjgbltkbjblt jrjbtjrjtrbgbjb jvjjtbrtb gvgvb.</p>
+        <a href="contact.php" class="btn2">contact us</a>
+</section>
+<footer class="footer">
+    <div class="inner-footer">
+        <div class="card">
+            <h3>about us</h3>
+            <ul>
+                <li>About us</li>
+                <li>Our Difference</li>
+                <li>Community Matters</li>
+                <li>Press</li>
+                <li>Bougs video</li>
+                <li>Blog</li>
+            </ul>
+        </div>
+        <div class="card">
+            <h3>help</h3>
+            <ul>
+                <li>flower care</li>
+                <li>help center</li>
+                <li>shipping</li>
+                <li>term of use</li>
+                <li>accessibility</li>
+                <li>privacy policy</li>
+            </ul>
+        </div>
+        <div class="card">
+            <h3>local</h3>
+            <ul>
+                <li>Kurunegala</li>
+                <li>Kandy</li>
+                <li>Colombo</li>
+                <li>Gampaha</li>
+                <li>Matara</li>
+                <li>Galle</li>
+            </ul>
+        </div>
+        <div class="card">
+            <h3>newsletter</h3>
+            <p>sign up for the latest ice offers and exclusive</p>
+            <div class="input-field">
+                <input type="text" name="" placeholder="Email Address...">
+                <i class="bi bi-envelope"></i>
+         </div> 
+        <div class="social-links">
+           <i class="bi bi-instagram"></i>
+           <i class="bi bi-youtube"></i>
+           <i class="bi bi-twitter"></i>
+           <i class="bi bi-whatsapp"></i>
+            </div>
+        </div>
+    </div>
+    <div  class="bottom-footer">
+        <p>all right reserved - code with selena</p>
+
+    </div>
+</footer>
+</body>
+</html>
