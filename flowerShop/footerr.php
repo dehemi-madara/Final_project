92d20800828cf92b78d822c9e83e6fe8c8b1eae8
dehemi-madara <dehemimadara@gmail.com>
2024-02-15 12:46:54 +0530

    updated

--- flowerShop/footerr.php
+++ flowerShop/footerr.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-    <script src="https://kit.fontawesome.com/95da0b33ad.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="mainC.css">
+    <script src="https://kit.fontawesome.com/95da0b33ad.js" 
+    crossorigin="anonymous"></script>
 </head>
 <body>
     <section class="home-contact">
         <h1>have any question?</h1>
-        <p>Lohjfegbr34mejo43r gfjokmjgbltkbjblt jrjbtjrjtrbgbjb jvjjtbrtb gvgvb.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus sed 
+            dicta necessitatibus minima, tempora magni?
+        </p>
         <a href="contact.php" class="btn2">contact us</a>
 </section>
-<footer class="footer">
+<footer class="footer-container">
     <div class="inner-footer">
         <div class="card">
             <h3>about us</h3>
@@ -53,7 +56,7 @@
             <p>sign up for the latest ice offers and exclusive</p>
             <div class="input-field">
                 <input type="text" name="" placeholder="Email Address...">
-                <i class="bi bi-envelope"></i>
+                <i class="fa-regular fa-envelope"></i>
          </div> 
         <div class="social-links">
         <i class="fa-brands fa-facebook"></i>
--- flowerShop/index.php
+++ flowerShop/index.php
@@ -16,10 +16,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
     crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="mainC.css">
+    <link rel="stylesheet" type="text/css" href="MainCategory.css">
+    
     <title>flower shop</title>
 </head>
 <body>
+
     <?php include 'header.php'; ?>
     <div class="slider-section">
         <div class="slider-show-container">
@@ -34,116 +37,154 @@
             </div>
         </div>
     </div>
-    <div>
-        <div class="row">
-            <div class="card">
-                <div class="details">
-                    <span>30% OFF TODAY</span>
-                    <h1>simple & elegant</h1>
-                    <a href="shop.php">shop now</a>
-                </div>
+    <div class="row">
+        <div class="card">
+            <div class="details">
+                <span>30% OFF TODAY</span>
+                <h1>simple & elegant</h1>
+                <a href="shop.php">shop now</a>
             </div>
-            <div class="card">
-                <div class="details">
-                    <span>30% OFF TODAY</span>
-                    <h1>simple & elegant</h1>
-                    <a href="shop.php">shop now</a>
-                </div>
+        </div>
+        <div class="card">
+            <div class="details">
+                <span>30% OFF TODAY</span>
+                <h1>simple & elegant</h1>
+                <a href="shop.php">shop now</a>
             </div>
-            <div class="card">
-                <div class="details">
-                    <span>30% OFF TODAY</span>
-                    <h1>simple & elegant</h1>
-                    <a href="shop.php">shop now</a>
-                </div>
+        </div>
+        <div class="card">
+            <div class="details">
+                <span>30% OFF TODAY</span>
+                <h1>simple & elegant</h1>
+                <a href="shop.php">shop now</a>
             </div>
         </div>
     </div>
     <div class="categories">
-        <h1 class = "title">TOP CATEGORIES</h1>
-        <div class="box-container">
-            <div class="box">
-                <img src="image/f.jpg">
-                <span>birthday</span>
-            </div>
-            <div class="box">
-                <img src="image/f.jpg">
-                <span>sympathy</span>
-            </div>
-            <div class="box">
-                <img src="image/f.jpg">
-                <span>next day</span>
-            </div>
-            <div class="box">
-                <img src="image/f.jpg">
-                <span>plant</span>
-            </div>
-            <div class="box">
-                <img src="image/f.jpg">
-                <span>wedding</span>
+            <h1 class = "title">TOP CATEGORIES</h1>
+            <div class="box-container">
+                <div class="box">
+                    <img src="image/birthdayC.jpg">
+                    <span>birthday</span>
+                </div>
+                <div class="box">
+                    <img src="image/sympathy.jpg">
+                    <span>sympathy</span>
+                </div>
+                <div class="box">
+                    <img src="image/nextDay.jpg">
+                    <span>next day</span>
+                </div>
+                <div class="box">
+                    <img src="image/plant.jpg">
+                    <span>plant</span>
+                </div>
+                <div class="box">
+                    <img src="image/wedding.jpg">
+                    <span>wedding</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="banner3">
-        <div class="detail">
-            <span>BETTER THAN CAKE</span>
-            <h1>BIRTHDAY BOUQES</h1>
-            <p>hiiiiii.......</p>
-            <a href="shop.php">explore<i class="fa-solid fa-arrow-right"></i></a>
+
+
+    <?php include 'footerr.php'; ?>
+   
+    
+
+        
+
+        <!-- 
+        
+        <div class="categories">
+            <h1 class = "title">TOP CATEGORIES</h1>
+            <div class="box-container">
+                <div class="box">
+                    <img src="image/f.jpg">
+                    <span>birthday</span>
+                </div>
+                <div class="box">
+                    <img src="image/f.jpg">
+                    <span>sympathy</span>
+                </div>
+                <div class="box">
+                    <img src="image/f.jpg">
+                    <span>next day</span>
+                </div>
+                <div class="box">
+                    <img src="image/f.jpg">
+                    <span>plant</span>
+                </div>
+                <div class="box">
+                    <img src="image/f.jpg">
+                    <span>wedding</span>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="shop">
-       <h1 class="title">shop besh sellers</h1> 
-       <?php
-       if(isset($message)) {
-        foreach ($message as $message) {
-            echo '
-            <div class="message"
-            <span>' .$message.'</span>
-            <i class="fa-solid fa-circle" onclick="this.parentElement.remove()"></i>
-            </div>';
-        }
-       }
-       ?>
-       <div class="box-container">
+
+        <div class="banner3">
+            <div class="detail">
+                <span>BETTER THAN CAKE</span>
+                <h1>BIRTHDAY BOUQES</h1>
+                <p>hiiiiii.......</p>
+                <a href="shop.php">explore<i class="fa-solid fa-arrow-right"></i></a>
+            </div>
+        </div>
+
+        <div class="shop">
+        <h1 class="title">shop besh sellers</h1> 
         <?php
-        $select_products = mysqli_query($conn,"SELECT * FROM 'products'") or die('query failed');
-        if(mysqli_num_rows($select_products)>0){
-            while($fetch_products=mysqli_fetch_assoc($select_products)){
-
-          
-        ?>
-        <form action="" method="post" class="box">
-            <img src="image/<?php echo $fetch_products['image']; ?>">
-            <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-            <div class="name">$<?php echo $fetch_products['name']; ?></div>
-            <input type="hidden" name="product_id" value="<?php echo $fetch_products['image']; ?>">
-            <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-            <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-            <input type="hidden" name="product_image" value="<?php echo $fetch_products['price']; ?>">
-            <div class="icon">
-                <a href="view_page.php?pid=<?php echo $fetch_products['image']; ?>" class="fa-solid fa-eye"></a>
-                <button type ="submit" name="add_to_wishlist" class="fa-solid fa-heart"></button>
-                <button type ="submit" name="add_to_cart" class="fa-solid fa-cart-shopping"></button>
-            </div>
-       </form>
-        <?php 
-                }
-            }else{
-                echo '<p class="empty">no products added yet !</p>';
+        if(isset($message)) {
+            foreach ($message as $message) {
+                echo '
+                <div class="message"
+                <span>' .$message.'</span>
+                <i class="fa-solid fa-circle" onclick="this.parentElement.remove()"></i>
+                </div>';
             }
-        
+        }
         ?>
-        
+        <div class="box-container">
+            <?php
+            $select_products = mysqli_query($conn,"SELECT * FROM 'products'") or die('query failed');
+            if(mysqli_num_rows($select_products)>0){
+                while($fetch_products=mysqli_fetch_assoc($select_products)){
 
-       </div>
-       <div class="more">
-        <a href="shop.php">load more</a>
-        <i class="fa-solid fa-arrow-down"></i>
-       </div>
-    </div>
+            
+            ?>
+            <form action="" method="post" class="box">
+                <img src="image/<?php echo $fetch_products['image']; ?>">
+                <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+                <div class="name">$<?php echo $fetch_products['name']; ?></div>
+                <input type="hidden" name="product_id" value="<?php echo $fetch_products['image']; ?>">
+                <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+                <input type="hidden" name="product_image" value="<?php echo $fetch_products['price']; ?>">
+                <div class="icon">
+                    <a href="view_page.php?pid=<?php echo $fetch_products['image']; ?>" class="fa-solid fa-eye"></a>
+                    <button type ="submit" name="add_to_wishlist" class="fa-solid fa-heart"></button>
+                    <button type ="submit" name="add_to_cart" class="fa-solid fa-cart-shopping"></button>
+                </div>
+        </form>
+            <?php 
+                    }
+                }else{
+                    echo '<p class="empty">no products added yet !</p>';
+                }
+            
+            ?>
+            
 
-    <?php include 'footer.php'; ?>
+        </div>
+        <div class="more">
+            <a href="shop.php">load more</a>
+            <i class="fa-solid fa-arrow-down"></i>
+        </div>
+        </div> -->
+
+
+   
     <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="scriptcloseedit.js"></script>
+    
 </body>
 </html>
